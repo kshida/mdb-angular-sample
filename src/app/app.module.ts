e3409dd40fd81189ac38cb520c1d1d0674d22fd0
kshida <34312716+kshida@users.noreply.github.com>
2020-11-23 20:19:16 +0900

    Navbarコンポーネントを追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavbarComponent,
     HomeComponent
   ],
   imports: [
